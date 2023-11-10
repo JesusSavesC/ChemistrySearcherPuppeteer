318a3024f37af4d9af46048d9fde676181fd698f
JesusSavesC <thomas.crawford@missionary.org>
2023-11-10 13:10:01 -0700

    Project Started

---
+++ src/index.js
@@ -0,0 +1,9 @@
+async function GetPage(A, B) {
+    const browswer = await pupeteer.launch({
+        headless:false,
+        args: ['--lang=en-GB']
+    })
+    const page = await browser.newPage();
+
+    await page.goto('https://kinetics.nist.gov/kinetics/KineticsSearchForm.jsp')
+}
