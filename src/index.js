8b7dd3e3fb3f73e2428401315bdd8d8295768cf3
JesusSavesC <thomas.crawford@missionary.org>
2023-11-10 15:51:02 -0700

    testing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+const puppeteer = require('puppeteer');
+
 async function GetPage(A, B) {
-    const browswer = await pupeteer.launch({
-        headless:false,
-        args: ['--lang=en-GB']
-    })
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://kinetics.nist.gov/kinetics/KineticsSearchForm.jsp');
+    await page.goto("https://google.com");
+    await page.screenshot({path: "amazing.png"});
+    await browser.close();
 }
+
+// Pass necessary arguments when calling the function
+GetPage(1, 1);
